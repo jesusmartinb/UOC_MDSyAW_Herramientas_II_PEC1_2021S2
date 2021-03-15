8225a053f7e9062e6e741fa79e448c70ddf6b05f
Jesús Martín Blanco <jmartinblan@uoc.edu>
2021-03-15 09:08:43 +0100

    Se comenta el archivo index.html y se traspasa la funcionalidad del boton hamburguesa al archivo main.js

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -14,3 +14,40 @@
   const university = "UOC";
   console.log(`Hello, ${university}!`);
 } )();
+
+
+/**
+ *
+ * Funcionalidad para el despliegue del menu con el botón hamburguesa
+ * para dispositivos moviles y tables con un tamaño inferior a los
+ * 730px de ancho
+ *
+ */
+
+const movil = window.matchMedia('screen and (min-width: 100px) and (max-width: 730px)');
+const boton = document.querySelector('#boton-menu');
+const menu = document.querySelector('.main-nav');
+const links = document.querySelectorAll('.main-nav__link');
+
+movil.addListener(validation);
+
+function validation(event){
+  if(event.matches){
+    boton.addEventListener('click', mostrarOcultarMenu);
+    links.forEach(function(link){
+      link.addEventListener('click', mostrarOcultarMenu);
+    });
+  }else{
+    boton.removeEventListener('click', mostrarOcultarMenu)
+  }
+}
+
+validation(movil);
+
+function mostrarOcultarMenu(){
+  if(menu.classList.contains('is-active')){
+    menu.classList.remove('is-active');
+  }else{
+    menu.classList.add('is-active');
+  }
+}
